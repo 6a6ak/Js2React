32a68c4e7ab83cb46f86f54e32d2e2a71b726afe
6a6ak <babak.ahari@gmail.com>
2024-05-09 13:04:15 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+// app.js
+console.log("Hello, World!");
+let name = "John";
+console.log("Name:", name);
+let number = 42;
+console.log("The answer to life, the universe, and everything is:", number);
