b9bbcdbea6b53744dd1368820c9b67f7e3be12d1
6a6ak <babak.ahari@gmail.com>
2024-05-09 13:27:36 +0200

    let var cons

---
+++ JS/var.js
@@ -0,0 +1,9 @@
+var height = 180;
+{
+    var weight = 70;
+    console.log(height); // -> 180
+    console.log(weight); // -> 70
+}
+console.log(height); // -> 180
+console.log(weight); // -> 70
+//var is globale but in a function
